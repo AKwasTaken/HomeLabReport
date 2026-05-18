fdfc8c804beb8cf9b40805c9f042f886c1c8e07b
Aneeth Kumaar <113939717+AKwasTaken@users.noreply.github.com>
2026-05-18 15:22:01 +0530

    bruh v3

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "AK's IOT",
+    pageTitle: "AK's HomeLabReport",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
